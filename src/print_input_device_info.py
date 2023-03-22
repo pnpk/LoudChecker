5394950802f7823e55f553f590c21f950452f337
pnpk <pnpk00@gmail.com>
2023-03-22 20:19:51 +0900

    インデント

--- src/print_input_device_info.py
+++ src/print_input_device_info.py
@@ -3,6 +3,7 @@ p = pyaudio.PyAudio()
 
 for index in range(0, p.get_device_count()):
     device_info = p.get_device_info_by_index(index)
-    print("DEVICE_INDEX:{}, DEVICE_NAME:{}".format(device_info["index"], device_info["name"]))
+    print("DEVICE_INDEX:{}, DEVICE_NAME:{}".format(
+        device_info["index"], device_info["name"]))
 
 p.terminate()
